7d57e4619d80544577f2fbcbc466490321e9b282
Emanuel Gonzalez <eg554848@gmail.coml>
2026-03-04 17:45:36 -0600

    API Hello World Spring Boot

---
+++ src/main/java/com/example/prueba/demo/service/HelloService.java
@@ -0,0 +1,11 @@
+package com.example.prueba.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getMessage() {
+        return "Hello World from Spring Boot!";
+    }
+}
