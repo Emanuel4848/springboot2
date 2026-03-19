c10afed0ce20e76259152c311991cc9aba7b1479
Emanuel Gonzalez <eg554848@gmail.coml>
2026-03-19 16:31:29 -0600

    prueba1 de automatizar prueb unitarias con terraform

--- src/test/java/com/example/prueba/demo/service/HelloServiceTests.java
+++ src/test/java/com/example/prueba/demo/service/HelloServiceTests.java
@@ -21,7 +21,7 @@ class HelloServiceTests {
     @DisplayName("Should return the correct greeting message")
     void testGetMessageReturnsCorrectMessage() {
         // Arrange
-        String expectedMessage = "Hello World from Spring Boot test4";
+        String expectedMessage = "Hello World from Spring Boot test3";
 
         // Act
         String actualMessage = helloService.getMessage();
