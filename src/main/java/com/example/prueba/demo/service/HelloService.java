3acd626d19d84115653f2e9a59da4f704e3f3634
Emanuel Gonzalez <eg554848@gmail.coml>
2026-03-19 20:06:04 -0600

    test14

--- src/main/java/com/example/prueba/demo/service/HelloService.java
+++ src/main/java/com/example/prueba/demo/service/HelloService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class HelloService {
 
     public String getMessage() {
-        return "Hello World from Spring Boot test12";
+        return "Hello World from Spring Boot test14";
     }
 }
