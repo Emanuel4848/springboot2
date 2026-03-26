5b58628fc6c7a83a33624f36b13c025c96b254a0
Emanuel Gonzalez <eg554848@gmail.coml>
2026-03-25 20:35:28 -0600

    test25

--- src/main/java/com/example/prueba/demo/service/HelloService.java
+++ src/main/java/com/example/prueba/demo/service/HelloService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class HelloService {
 
     public String getMessage() {
-        return "Hello World from Spring Boot test24";
+        return "Hello World from Spring Boot test25";
     }
 }
--- src/test/java/com/example/prueba/demo/service/HelloServiceTests.java
+++ src/test/java/com/example/prueba/demo/service/HelloServiceTests.java
@@ -21,7 +21,7 @@ class HelloServiceTests {
     @DisplayName("Should return the correct greeting message")
     void testGetMessageReturnsCorrectMessage() {
         // Arrange
-        String expectedMessage = "Hello World from Spring Boot test24";
+        String expectedMessage = "Hello World from Spring Boot test25";
 
         // Act
         String actualMessage = helloService.getMessage();
